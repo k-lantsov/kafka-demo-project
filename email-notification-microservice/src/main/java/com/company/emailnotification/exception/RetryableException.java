df45dad844fb3482cbad1ddf95173ea74b5b089f
k.lantsov <k.lantsov@yahoo.com>
2024-01-23 22:16:43 +0300

    email-notification-microservice creating retryable and not retryable exceptions

---
+++ email-notification-microservice/src/main/java/com/company/emailnotification/exception/RetryableException.java
@@ -0,0 +1,12 @@
+package com.company.emailnotification.exception;
+
+public class RetryableException extends RuntimeException {
+
+    public RetryableException(String message) {
+        super(message);
+    }
+
+    public RetryableException(Throwable cause) {
+        super(cause);
+    }
+}
